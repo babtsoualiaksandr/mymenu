12f2c8e0602bcbcbcd0d18d84c54893e6fd1bf4f
Aliaksandr Babtsou <babtsoualiaksandr@gmail.com>
2023-04-01 11:20:23 +0300

    made: Task

--- mymenu/urls.py
+++ mymenu/urls.py
@@ -15,7 +15,14 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path
+from menu import views
+from django.conf import settings
+from django.conf.urls.static import static
+
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-]
+    path('<name>', views.base, ),
+] + static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
+print(settings.STATIC_URL)
+print(settings.STATIC_ROOT)
